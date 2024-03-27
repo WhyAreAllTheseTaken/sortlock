e5e0d41353b8a3deb54d879759387d3a2eebc3e9
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-03-27 10:25:48 +0000

    Document SortKey.

--- src/key.rs
+++ src/key.rs
@@ -4,11 +4,13 @@ use std::sync::atomic::{AtomicUsize, Ordering};
 static NEXT_KEY: AtomicUsize = AtomicUsize::new(0);
 
 /// A sort key for sorting locks.
+///
+/// This must be unique to each lock.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct SortKey(usize);
 
 impl SortKey {
-    /// Creates a new sort key.
+    /// Creates a new unique sort key.
     pub fn new() -> Self {
         Self(NEXT_KEY.fetch_add(1, Ordering::Relaxed))
     }
